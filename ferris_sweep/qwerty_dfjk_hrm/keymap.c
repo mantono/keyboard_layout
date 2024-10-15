46dbd8ee66c2f3c0781d5f90b7e47cc2db26cfc9
Anton Österberg <anton.osterberg@protonmail.com>
2024-10-15 22:49:53 +0200

    Actually add keys to SWE layer

--- ferris_sweep/qwerty_dfjk_hrm/keymap.c
+++ ferris_sweep/qwerty_dfjk_hrm/keymap.c
@@ -194,10 +194,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Swedish letters
     [SWE] = LAYOUT_split_3x5_2(
-                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                                KC_NO, KC_NO,              KC_NO, KC_NO
+                           KC_NO, KC_NO,     KC_NO,     KC_NO,     KC_NO,              KC_NO,     KC_NO,     KC_NO, KC_NO, KC_NO,
+                           KC_NO, KC_NO, TD(TD_OE), TD(TD_AE), TD(TD_AA),          TD(TD_AA), TD(TD_AE), TD(TD_OE), KC_NO, KC_NO,
+                           KC_NO, KC_NO,     KC_NO,     KC_NO,     KC_NO,              KC_NO,     KC_NO,     KC_NO, KC_NO, KC_NO,
+                                                        KC_NO,     KC_NO,              KC_NO,     KC_NO
                            )
 };
 
